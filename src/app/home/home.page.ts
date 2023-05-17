7dd09a627d6ff37bde1b7db47a9b471693310c59
Eduardo Gigliotti <99850450+GigliottiEduardo@users.noreply.github.com>
2023-05-16 22:15:30 -0300

    Subindo Arquivos

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,35 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GoogleMap } from '@capacitor/google-maps';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  @ViewChild('map') 
+  mapRef!: ElementRef<HTMLElement>; 
+  Map!: GoogleMap;
+
+  constructor() {}
+
+  ionViewWillEnter() {
+    this.createMap(); 
+  }
+
+  async createMap() {
+    this.Map = await GoogleMap.create({
+      id: 'my-map',
+      element: this.mapRef.nativeElement,
+      apiKey: environment.mapsKey,
+      config: {
+        center: {
+          lat: 33.6,
+          lng: -117.9,
+        },
+        zoom: 8,
+      },
+    });
+  }
+}
